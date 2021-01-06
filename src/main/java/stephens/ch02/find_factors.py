c6ef78bd47c873b00650333880ebe27f86967f95
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-07 01:42:41 +0300

    Update find_factors.py

--- src/main/java/stephens/ch02/find_factors.py
+++ src/main/java/stephens/ch02/find_factors.py
@@ -4,5 +4,11 @@ from typing import List
 def version01(number: int) -> List[int]:
     factors: List[int] = []
     i: int = 2
-
+    while i < number:
+        while number % i == 0:
+            factors.append(i)
+            number = number / i
+        i += 1
+    if number > 1:
+        factors.append(number)
     return factors
