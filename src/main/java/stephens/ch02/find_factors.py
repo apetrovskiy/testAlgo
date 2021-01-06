0b8f897124a5a9d3a8946277f5ead69892fed2fb
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-07 01:45:05 +0300

    version 01 is done

--- src/main/java/stephens/ch02/find_factors.py
+++ src/main/java/stephens/ch02/find_factors.py
@@ -12,3 +12,16 @@ def version01(number: int) -> List[int]:
     if number > 1:
         factors.append(number)
     return factors
+
+
+def version02(number: int) -> List[int]:
+    factors: List[int] = []
+    i: int = 2
+    while i < number:
+        while number % i == 0:
+            factors.append(i)
+            number = number / i
+        i += 1
+    if number > 1:
+        factors.append(number)
+    return factors
--- src/test/java/stephens/ch02/test_find_factors.py
+++ src/test/java/stephens/ch02/test_find_factors.py
@@ -6,10 +6,16 @@ import pytest
 test_data = [
     (4, [2, 2]),
     (10, [2, 5]),
-    (125, [5, 5, 5])
+    (125, [5, 5, 5]),
+    (127, [127])
 ]
 
 
 @pytest.mark.parametrize("input,expected_result", test_data)
-def test_find_factors(input: int, expected_result: List[int]):
+def test_find_factors_version01(input: int, expected_result: List[int]):
     assert expected_result == version01(input)
+
+
+@pytest.mark.parametrize("input,expected_result", test_data)
+def test_find_factors_version02(input: int, expected_result: List[int]):
+    assert expected_result == version02(input)
