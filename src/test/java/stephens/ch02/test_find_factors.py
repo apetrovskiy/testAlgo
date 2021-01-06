3ad96af728d366012a0bfbccca8f59524e1c1240
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-07 00:51:35 +0300

    Update test_find_factors.py

--- src/test/java/stephens/ch02/test_find_factors.py
+++ src/test/java/stephens/ch02/test_find_factors.py
@@ -1,2 +1,2 @@
-test_data = []@pytest.mark.parametrize("input,expected_reuslt", test)def test_find_factors(input: int, expected_reuslt:List[int]):
-    
+test_data = []@pytest.mark.parametrize("input,expected_reuslt", test_data)def test_find_factors(input: int, expected_reuslt:List[int]):
+    assert expeted_result = find_factors(input)
