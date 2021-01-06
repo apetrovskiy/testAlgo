f6fc7fb40a2e561cac3f032cf1df7dd127671cb3
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-07 01:32:51 +0300

    reformatting

--- src/main/java/stephens/ch02/find_factors.py
+++ src/main/java/stephens/ch02/find_factors.py
@@ -1,1 +1,8 @@
-def version01(number: int):factors: List[int] = []i:int=2
+from typing import List
+
+
+def version01(number: int) -> List[int]:
+    factors: List[int] = []
+    i: int = 2
+
+    return factors
--- src/test/java/stephens/ch02/test_find_factors.py
+++ src/test/java/stephens/ch02/test_find_factors.py
@@ -1,2 +1,15 @@
-test_data = []@pytest.mark.parametrize("input,expected_reuslt", test_data)def test_find_factors(input: int, expected_reuslt:List[int]):
-    assert expeted_result = find_factors(input)
+from src.main.java.stephens.ch02.find_factors import version01
+from typing import List
+import pytest
+
+
+test_data = [
+    (4, [2, 2]),
+    (10, [2, 5]),
+    (125, [5, 5, 5])
+]
+
+
+@pytest.mark.parametrize("input,expected_result", test_data)
+def test_find_factors(input: int, expected_result: List[int]):
+    assert expected_result == version01(input)
