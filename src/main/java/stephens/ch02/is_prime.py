add441cf020a4904db46cd6df38fb2adfd5f458e
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-01-07 07:50:18 +0300

    .

--- src/main/java/stephens/ch02/is_prime.py
+++ src/main/java/stephens/ch02/is_prime.py
@@ -1,2 +1,9 @@
-def is_prime(p:int,max_tests:int)->bool:
-    for test in range(1,max_tests + 1):if randint(2,p - 1) % p != 1:return Falsereturn True
+from random import randint
+
+
+def is_prime(p: int, max_tests: int) -> bool:
+    for test in range(1, max_tests + 1):
+        if randint(2, p - 1) % p != 1:
+        # if randint(1, p) % p != 1:
+            return False
+    return True
--- src/test/java/stephens/ch02/test_is_prime.py
+++ src/test/java/stephens/ch02/test_is_prime.py
@@ -1,2 +1,15 @@
+from src.main.java.stephens.ch02.is_prime import is_prime
 import pytest
-test_data = []@pytest.mark.parametrize("number,tests_number,expected_result", test_data)def test_is_prime(numbert:int, tests_number:int, expected_result:bool):assert expected_result == is_prime(number, tests_number)
+
+
+test_data = [
+    (5, 10, True),
+    (6, 2, False),
+    (27, 5, False),
+    (37, 20, True)
+]
+
+
+@pytest.mark.parametrize("number,tests_number,expected_result", test_data)
+def test_is_prime(number: int, tests_number: int, expected_result: bool):
+    assert expected_result == is_prime(number, tests_number)
