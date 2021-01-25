3e572ef4b2727c064ada37060e99cad9f90f1b0c
Alexander <apetrovskiy@hotmail.com>
2021-01-26 01:46:12 +0300

    Update anagrams.py

--- src/main/java/durr_vie/CharacterStrings/Anagrams/anagrams.py
+++ src/main/java/durr_vie/CharacterStrings/Anagrams/anagrams.py
@@ -1,5 +1,22 @@
+from collections import Counter
 from typing import List
 
 
-def anagrams(input: str) -> List[str]:
+def version01_clean_python(input: str) -> List[List[str]]:
+    input_list = input.split(' ')
+    print(input_list)
+    l = [(x, ''.join(sorted(x))) for x in input_list]
+    print(l)
+    dict1 = dict(l)
+    print(dict1)
+    c = Counter(l)
+    print(c)
+    l2 = [(''.join(sorted(x)), x) for x in input_list]
+    print(l2)
+    dict2 = dict(l2)
+    print(dict2)
     return []
+
+
+def anagrams(input: str) -> List[str]:
+    return version01_clean_python(input)
