c4b56f7c587e1084fb00e9e1547aa374ecb0cad4
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-01-26 03:14:14 +0300

    2.1 Anagrams is done

--- src/main/java/durr_vie/CharacterStrings/Anagrams/anagrams.py
+++ src/main/java/durr_vie/CharacterStrings/Anagrams/anagrams.py
@@ -2,7 +2,7 @@ from collections import Counter
 from typing import List
 
 
-def version01_clean_python(input: str) -> List[List[str]]:
+def version01_clean_python_mine(input: str) -> List[List[str]]:
     input_list = input.split(' ')
     print(input_list)
     l = [(x, ''.join(sorted(x))) for x in input_list]
@@ -18,5 +18,28 @@ def version01_clean_python(input: str) -> List[List[str]]:
     return []
 
 
+def version02_clean_python_kite(input: str) -> List[List[str]]:
+    input_list = input.split(' ')
+    list_of_lists = [[x, ''.join(sorted(x))] for x in input_list]
+    all_values = [list[1] for list in list_of_lists]
+    unique_values = set(all_values)
+
+    group_list = []
+    for value in unique_values:
+        this_group = []
+        for list in list_of_lists:
+            if list[1] == value:
+                this_group.append(list[0])
+        group_list.append(this_group)
+    reduced_grouped_list = [x for x in group_list if len(x) > 1 and x[0] != x[1]]
+
+    return reduced_grouped_list
+
+
+def version03_itertools_kite(intpu: str) -> List[List[str]]:
+    return []
+
+
 def anagrams(input: str) -> List[str]:
-    return version01_clean_python(input)
+    # return version01_clean_python_mine(input)
+    return version02_clean_python_kite(input)
