01dbf3ecd3a54b48bce8d316db693fc2f959c139
Alexander <apetrovskiy@hotmail.com>
2021-01-31 14:28:16 +0300

    in progress

---
+++ src/main/java/fox/lesson011/palindromes.py
@@ -0,0 +1,5 @@
+def is_palindrome(s: str):
+    if len(s) <= 1:
+        return True
+    # else:
+        
---
+++ src/test/java/fox/lesson011/test_towers.py
@@ -0,0 +1,14 @@
+from src.main.java.fox.lesson011.towers import move
+import pytest
+
+
+test_data = [
+    (3, 1, 3, 2)
+]
+
+
+@pytest.mark.parametrize("n,start,finish,expected_result", test_data)
+def test_move(n, start, finish, expected_result):
+    print("| N ring | start | finish |")
+    # assert expected_result == move(n, start, finish)
+    move(n, start, finish)
