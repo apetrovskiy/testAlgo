f917ce7fe44c919611d16548b955dbbbb9b5a446
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-02-17 13:57:06 +0300

    Update test_primes.py

--- src/test/java/fox/lesson012/test_primes.py
+++ src/test/java/fox/lesson012/test_primes.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.main.java.fox.lesson012.primes import is_prime_number
 import pytest
 
@@ -7,7 +8,14 @@ test_data = [
     (15, False)
 ]
 
+test_data_for_series = [(35, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])]
+
 
 @pytest.mark.parametrize("input,expected_result", test_data)
 def test_is_prime_number(input: int, expected_result: int):
     assert expected_result == is_prime_number(input)
+
+
+@pytest.mark.parametrize("input,expected_result", test_data_for_series)
+def test_primes_series(input: int, expected_result: List[int]):
+    assert expected_result == [x for x in range(2, input) if is_prime_number(x)]
