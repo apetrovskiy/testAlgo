1ca9f574cb7f2122f60283b486d2b59a22b64eea
Aleksandr Petrovskii <aleksandr_petrovskii@epam.com>
2021-02-19 10:07:08 +0300

    fixes and formatting

--- src/test/java/testAlgo/fox/lesson012/test_primes.py
+++ src/test/java/testAlgo/fox/lesson012/test_primes.py
@@ -8,13 +8,17 @@ test_data = [
     (15, False)
 ]
 
-test_data_for_series = [(35, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])]
+test_data_for_series = [
+    (35, [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31])
+]
 
-test_data_for_sum = [32, 4096, 1069931]
+test_data_for_sum = [
+    (32, 4096, 1069931)
+]
 
 
 @pytest.mark.parametrize("input,expected_result", test_data)
-def test_is_prime_number(input: int, expected_result: int):
+def test_is_prime_number(input: int, expected_result: bool):
     assert expected_result == is_prime_number(input)
 
 
@@ -25,6 +29,6 @@ def test_primes_series(input: int, expected_result: List[int]):
 
 
 @pytest.mark.parametrize("start,end,expected_result", test_data_for_sum)
-def test_primes_sum(start: int, end: int, expected_result: List[int]):
+def test_primes_sum(start: int, end: int, expected_result: int):
     assert expected_result == sum(
-        [x for x in range(start, end+1) if is_prime_number(x)])
+        [x for x in range(start, end + 1) if is_prime_number(x)])
