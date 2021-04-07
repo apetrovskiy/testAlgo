cd2ca3304084f2abd514dec11c6cb1cb517e4f5d
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-07 10:32:50 +0300

    Update max_value.py

--- src/main/java/testAlgo/yang_hu/max_value.py
+++ src/main/java/testAlgo/yang_hu/max_value.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def max_value(input_array: List[int]):
-    print(f"length = {len(input_array)}")
+    # print(f"length = {len(input_array)}")
     for i in range(len(input_array)):
         # print(i)
         if i < len(input_array)-2 and input_array[i] > input_array[i + 1]:
