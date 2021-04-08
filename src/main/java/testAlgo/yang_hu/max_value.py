e27724d1a0bc6dd2bd63b04b9ce574ce278afb6e
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-09 01:08:15 +0300

    Update max_value.py

--- src/main/java/testAlgo/yang_hu/max_value.py
+++ src/main/java/testAlgo/yang_hu/max_value.py
@@ -5,6 +5,6 @@ def max_value(input_array: List[int]):
     # print(f"length = {len(input_array)}")
     for i in range(len(input_array)):
         # print(i)
-        if i < len(input_array)-2 and input_array[i] > input_array[i + 1]:
+        if i < len(input_array)-1 and input_array[i] > input_array[i + 1]:
             input_array[i], input_array[i+1] = input_array[i+1], input_array[i]
     return input_array[len(input_array)-1]
