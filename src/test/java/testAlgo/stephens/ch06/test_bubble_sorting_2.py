36578bdc6c947f5c1e9bef844c32e6afb9b7399b
Alexander <apetrovskiy@hotmail.com>
2021-04-13 13:35:56 +0300

    preparation

---
+++ src/test/java/testAlgo/stephens/ch06/test_bubble_sorting_2.py
@@ -0,0 +1,13 @@
+import pytest
+from typing import List
+from src.main.java.testAlgo.stephens.ch06.bubble_sorting_2 import bubble_sorting
+
+
+test_data = [
+    ([0, 1, 2, 4, 5, 6, 3, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+]
+
+
+@pytest.mark.parametrize("input_array,expected_result", test_data)
+def test_bubble_sorting_2(input_array: List[int], expected_result: List[int]):
+    assert expected_result == bubble_sorting(input_array)
---
+++ src/test/java/testAlgo/stephens/ch06/test_bucket_sorting_2.py
@@ -0,0 +1,13 @@
+import pytest
+from typing import List
+from src.main.java.testAlgo.stephens.ch06.bucket_sorting_2 import bucket_sorting
+
+
+test_data = [
+    ([0, 1, 2, 4, 5, 6, 3, 7, 8, 9], [0, 1, 2, 3, 4, 5, 6, 7, 8, 9])  # ??
+]
+
+
+@pytest.mark.parametrize("input_array,expected_result", test_data)
+def test_bucket_sorting_2(input_array: List[int], expected_result: List[int]):
+    assert expected_result == bucket_sorting(input_array)
