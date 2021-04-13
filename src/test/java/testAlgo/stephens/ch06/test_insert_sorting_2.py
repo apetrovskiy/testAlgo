ad7e98125da86f9d14879f3ce02c90c605c38f41
Alexander <apetrovskiy@hotmail.com>
2021-04-13 08:13:28 +0300

    next changes

--- src/test/java/testAlgo/stephens/ch06/test_insert_sorting_2.py
+++ src/test/java/testAlgo/stephens/ch06/test_insert_sorting_2.py
@@ -1,10 +1,10 @@
 import pytest
 from typing import List
-from src.main.java.testAlgo.stephens.ch06.insert_sorting import insert_sorting
+from src.main.java.testAlgo.stephens.ch06.insert_sorting_2 import insert_sorting
 
 
 test_data = [
-    ([], [])
+    ([7, 5, 6, 1, 3, 2, 8, 0], [0, 1, 2, 3, 5, 6, 7, 8])
 ]
 
 
