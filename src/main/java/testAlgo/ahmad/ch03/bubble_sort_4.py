e88ec1298ffae906f297daa078ef4e9a1d640fe4
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-14 19:24:47 +0300

    preparation

---
+++ src/main/java/testAlgo/ahmad/ch03/bubble_sort_4.py
@@ -0,0 +1,6 @@
+from typing import List
+
+
+def bubble_sort(input_array: List[int])-> List[int]:
+    
+    return input_array
---
+++ src/test/java/testAlgo/ahmad/ch03/test_bubble_sort_4.py
@@ -0,0 +1,12 @@
+import pytest
+from typing import List
+from src.main.java.testAlgo.ahmad.ch03.bubble_sort_4 import bubble_sort
+
+
+test_data=[
+    ([25,21,22,24,23,27,26],[21,22,23,24,25,26,27])
+]
+
+@pytest.mark.parametrize("input_array,expected_result",test_data)
+def test_bubble_sort(input_array: List[int], expected_result: List[int]):
+    assert expected_result==bubble_sort(input_array)
