7e37b2db63f9fd1ead66d36812a17854f10b73bc
Alexander-Petrovskiy <apetrovskiy2@hotmail.com>
2021-04-15 09:42:40 +0300

    bubble sort, insertion sort

--- src/main/java/testAlgo/ahmad/ch03/bubble_sort_4.py
+++ src/main/java/testAlgo/ahmad/ch03/bubble_sort_4.py
@@ -6,6 +6,6 @@ def bubble_sort(input_array: List[int]) -> List[int]:
     for pass_no in range(last_element_index, 0, -1):
         for index in range(pass_no):
             if input_array[index] > input_array[index+1]:
-                input_array[index], input_array[index +
-                                                1] = input_array[index+1], input_array[index]
+                input_array[index], input_array[index + 1] = \
+                    input_array[index+1], input_array[index]
     return input_array
