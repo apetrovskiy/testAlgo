fef6460d8bcfaaf6111a5e6d14badb9dc330aa56
Aleksandr Petrovskii <uiautomation@hotmail.com>
2021-04-16 08:33:14 +0300

    tests have been skipped

--- src/test/java/testAlgo/stephens/ch02/test_is_prime.py
+++ src/test/java/testAlgo/stephens/ch02/test_is_prime.py
@@ -13,7 +13,7 @@ test_data = [
 
 @allure.feature('Stephens')
 @allure.story('Is prime')
-# @pytest.mark.skip(reason="TODO: no way of currently testing this")
+@pytest.mark.skip(reason="TODO: no way of currently testing this")
 @pytest.mark.parametrize("number,tests_number,expected_result", test_data)
 def test_is_prime(number: int, tests_number: int, expected_result: bool):
     assert expected_result == is_prime(number, tests_number)
