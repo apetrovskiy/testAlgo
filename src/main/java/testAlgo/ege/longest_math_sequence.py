8111ff72640473ffd2906f1d3c87f2cea4a82619
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2024-06-22 23:56:40 +0300

    maximum math sequence (ege) is done

--- src/main/java/testAlgo/ege/longest_math_sequence.py
+++ src/main/java/testAlgo/ege/longest_math_sequence.py
@@ -12,8 +12,20 @@ def longest_math_sequence_by_lanskaya(s: str):
     return len(ans) - 1
 
 
-# def longest_math_sequence(s: str):
-#     if len(s) <= 1:
-#         return True
-#     else:
-#         return s[0] == s[-1] and is_palindrome(s[1:-1])
+def longest_math_sequence(s: str):
+    max_length = 0
+    current_length = 0
+    previous_char = ""
+    for current_char in s:
+        if current_char in "0123456789" and previous_char == "0":
+            max_length = current_length if current_length > max_length else max_length
+            current_length = 1
+        elif current_char in "-*" and previous_char in "-*":
+            current_length = 0
+        else:
+            current_length += 1
+        previous_char = current_char
+    if s[-1:] in "-*":
+        current_length -= 1
+    max_length = current_length if current_length > max_length else max_length
+    return max_length
--- src/test/java/testAlgo/ege/test_longest_math_sequence.py
+++ src/test/java/testAlgo/ege/test_longest_math_sequence.py
@@ -2,11 +2,14 @@ import allure
 import pytest
 
 from src.main.java.testAlgo.ege.longest_math_sequence import (
+    longest_math_sequence,
     longest_math_sequence_by_lanskaya,
 )
 
 test_data = [
     ("00", 1),
+    ("010", 2),
+    ("1*", 1),
     ("01*1", 3),
     ("123-4*324-0123-4*324-", 11),
     ("", 0),
@@ -19,6 +22,16 @@ test_data = [
 @pytest.mark.parametrize("input_data,expected_result", test_data)
 def test_longest_math_sequence_by_lanskaya(input_data: str, expected_result: int):
     actual = longest_math_sequence_by_lanskaya(input_data)
+    assert (
+        expected_result == actual
+    ), f"For '{input_data}' expected {expected_result}, actual {actual}"
+
+
+@allure.feature("EGE")
+@allure.story("Math sequence")
+@pytest.mark.parametrize("input_data,expected_result", test_data)
+def test_longest_math_sequence(input_data: str, expected_result: int):
+    actual = longest_math_sequence(input_data)
     assert (
         expected_result == actual
     ), f"For '{input_data}' expected {expected_result}, actual {actual}"
