986c92a18c1514809832d0566bc1a9c5f8f6cde0
Alexander Petrovskiy <petrovskiy.alexander@gmail.com>
2024-06-26 16:14:01 +0300

    has finished, probably

--- update/init_poetry.sh
+++ update/init_poetry.sh
@@ -12,13 +12,15 @@ rm -rf ~/Library/Caches/pypoetry/cache
 poetry env use python3.12
 # https://github.com/python-poetry/poetry/issues/1422
 # run
-# poetry new --src ${PACKAGE_FOLDER_NAME} --name ${PACKAGE_NAME}
+# poetry new --src ${PACKAGE_FOLDER_NAME} --name ${PACKAGE_FOLDER_NAME}
 # cd ${PACKAGE_FOLDER_NAME} || exit
 # then rename ${PACKAGE_FOLDER_NAME} and run
-# poetry init --name ${PACKAGE_NAME} --quiet
+# poetry init --name ${PACKAGE_FOLDER_NAME} --quiet
 # finally, merge ${PACKAGE_FOLDER_NAME}/pyproject.toml into ./pyproject.toml,
 # namely copy-paste the following:
 # packages = [{include = "main", from = "src"}]
+# instead of
+# packages = [{include = "testalgo", from = "src"}]
 
 poetry add structlog
 poetry add --group test pytest allure-pytest pytest-xdist typing-extensions pytest-test-groups --allow-prereleases &&
